f04c2b697a0210939185270f8100e20a817471e7
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-11-19 21:54:53 +0530

    Update operator.cpp

--- love b./video 6/bitwise/operator.cpp
+++ love b./video 6/bitwise/operator.cpp
@@ -14,20 +14,11 @@ int main(){
     return 0;
 }
 
-output of the code::
+/**output of the code::**/
 
-#include<iostream>
-using namespace std;
-
-int main(){
-    int n;
-    cin>>n;
-    int a = 3;
-
-    cout << (n&a) << " n&n" <<endl;
-    cout << (n|n) << " n|n" <<endl;
-    cout << ~n  << " ~n " <<endl;
-    cout << (n^n) << " n^n" <<endl;
-    
-    return 0;
+2
+2 n&n
+2 n|n
+-3 ~n 
+0 n^n
 }
