73ed8c358a34889c89d51c99c5f45609552c57d9
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-11-19 22:11:07 +0530

    Create if and else .cpp

---
+++ if and else .cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b=1;
+    a= 10;
+    
+    if(++a)
+        cout<< b;
+    else
+        cout<< ++b;
+    
+    cout<<"Hello World";
+
+    return 0;
+}
+
+
+output is  1;
+
+becouse that the  "if" me agar koi value put ki jati hai to vo only one times hi operate hoga
+but if the condition honi chahiye.
