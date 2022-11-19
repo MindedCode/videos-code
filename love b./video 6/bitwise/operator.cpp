ea1c082c0007855c65ec4f5ee08c65c7d73b73c8
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-11-19 21:53:53 +0530

    Create operator.cpp

---
+++ love b./video 6/bitwise/operator.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a = 3;
+
+    cout << (n&a) << " n&n" <<endl;
+    cout << (n|n) << " n|n" <<endl;
+    cout << ~n  << " ~n " <<endl;
+    cout << (n^n) << " n^n" <<endl;
+    
+    return 0;
+}
+
+output of the code::
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a = 3;
+
+    cout << (n&a) << " n&n" <<endl;
+    cout << (n|n) << " n|n" <<endl;
+    cout << ~n  << " ~n " <<endl;
+    cout << (n^n) << " n^n" <<endl;
+    
+    return 0;
+}
