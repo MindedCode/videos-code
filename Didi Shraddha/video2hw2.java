d51ed2b96a263fe900de4ecaffd321de80d73051
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-10 12:33:59 +0530

    number of table using take user input.
    Q --- Make a program that prints the table of a number that is input by the user.

--- Didi Shraddha/video2hw2.java
+++ Didi Shraddha/video2hw2.java
@@ -26,7 +26,51 @@ public class Main
 	}
 }
 
+
+2---------------------------------------------------------------------------------------
+	/******************************************************************************
+Q --- Make a program that prints the table of a number that is input by the user. 
+
+*******************************************************************************/
+
+// Code here :
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+        System.out.println("the number of table => " + number);
+        
+        // int i = 1;
+        // while(i<=10){
+            // int table = number*1;
+            
+
+            
+            
+            System.out.println(number + " * " + 1 +" = " + number*1 );
+            System.out.println(number + " * " + 2 +" = " + number*2 );
+            System.out.println(number + " * " + 3 +" = " + number*3 );
+            System.out.println(number + " * " + 4 +" = " + number*4 );
+            System.out.println(number + " * " + 5 +" = " + number*5 );
+            System.out.println(number + " * " + 6 +" = " + number*6 );
+            System.out.println(number + " * " + 7 +" = " + number*7 );
+            System.out.println(number + " * " + 8 +" = " + number*8 );
+            System.out.println(number + " * " + 9 +" = " + number*9 );
+            System.out.println(number + " * " + 10 +" = " + number*10 );
+        //     i++;
+        // }
+	}
+}
+
+
 **/Output of the program :/**
+
 6
 the number of table => 6
 6 * 1 = 6
