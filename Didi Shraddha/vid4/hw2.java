d72abbb2bfcf2be2df5e7e43edb4b78367bd6107
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-11 05:52:26 +0530

    for(;;)
    for(;;) {
                 System.out.println("Tony");
             }

---
+++ Didi Shraddha/vid4/hw2.java
@@ -0,0 +1,22 @@
+// // for(;;) {
+//             System.out.println("Tony");
+//         }
+
+import java.util.*;
+public class Main{
+    public static void main (String[] args) {
+        for(;;) {
+            System.out.println("Tony");
+        }
+
+    }
+}
+
+**/output : /**
+Tony
+Tony
+.
+.
+.
+.
+infinte loop when depend the memory size.
