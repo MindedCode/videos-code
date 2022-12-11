d13dc265a0d87fb9db173274349a2c256842d133
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-11 05:47:48 +0530

    using for loop // Print all even numbers till n.
    // Print all even numbers till n.

---
+++ Didi Shraddha/vid4/hw1.java
@@ -0,0 +1,19 @@
+// Print all even numbers till n.
+
+import java.util.*;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        for(int i=1; i<=num; i++){
+            if(i%2==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+**/Output of the program: /**
+
+2 4 6 8 10 12 14 16 18 20
