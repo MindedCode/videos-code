3ac05ec7aa96887b4bd1c0636f594d904f456215
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-12 08:20:31 +0530

    butterfly pattern

--- Didi Shraddha/vid6/vid solve with problems/butterfly pattern.java
+++ Didi Shraddha/vid6/vid solve with problems/butterfly pattern.java
@@ -1,5 +1,9 @@
 import java.util.*;
  
+
+
+import java.util.*;
+ 
 public class Main {
    public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
@@ -8,12 +12,8 @@ public class Main {
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
-            // first space
-            for(int j=n-i; j>=1; j--){
-                System.out.print("  ");
-            }
-            // second space
-            for(int j=n-i; j>=1; j--){
+            // upper spaces
+            for(int j=2*(n-i); j>=1; j--){
                 System.out.print("  ");
             }
             // last upper inverted pyramid triangle
@@ -27,12 +27,8 @@ public class Main {
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
-            // first space
-            for(int j=n-i; j>=1; j--){
-                System.out.print("  ");
-            }
-            // second space
-            for(int j=n-i; j>=1; j--){
+            // down spaces
+            for(int j=2*(n-i); j>=1; j--){
                 System.out.print("  ");
             }
             // last down inverted pyramid triangle
@@ -45,6 +41,54 @@ public class Main {
         System.out.println("\n\nButterfly pattern");
    }   
 }
+   
+
+// second methed
+// public class Main {
+//    public static void main(String args[]) {
+//        Scanner sc = new Scanner(System.in);
+//        int n=sc.nextInt();
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("* ");
+//             }
+//             // first space
+//             for(int j=n-i; j>=1; j--){
+//                 System.out.print("  ");
+//             }
+//             // second space
+//             for(int j=n-i; j>=1; j--){
+//                 System.out.print("  ");
+//             }
+//             // last upper inverted pyramid triangle
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println(" ");
+//         }
+//         // downpattern ----------------------------------------------------------
+//         for(int i=n; i>=1; i--){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("* ");
+//             }
+//             // first space
+//             for(int j=n-i; j>=1; j--){
+//                 System.out.print("  ");
+//             }
+//             // second space
+//             for(int j=n-i; j>=1; j--){
+//                 System.out.print("  ");
+//             }
+//             // last down inverted pyramid triangle
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println(" ");
+//         }
+        
+//         System.out.println("\n\nButterfly pattern");
+//    }   
+// }
 
 
 **/Output of the Program::/**
