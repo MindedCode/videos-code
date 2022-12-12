1030898bec4753ee3ced72eefebf4e929885b947
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-12 08:11:26 +0530

    Create butterfly pattern.java

---
+++ Didi Shraddha/vid6/vid solve with problems/butterfly pattern.java
@@ -0,0 +1,65 @@
+import java.util.*;
+ 
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            // first space
+            for(int j=n-i; j>=1; j--){
+                System.out.print("  ");
+            }
+            // second space
+            for(int j=n-i; j>=1; j--){
+                System.out.print("  ");
+            }
+            // last upper inverted pyramid triangle
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        // downpattern ----------------------------------------------------------
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            // first space
+            for(int j=n-i; j>=1; j--){
+                System.out.print("  ");
+            }
+            // second space
+            for(int j=n-i; j>=1; j--){
+                System.out.print("  ");
+            }
+            // last down inverted pyramid triangle
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        
+        System.out.println("\n\nButterfly pattern");
+   }   
+}
+
+
+**/Output of the Program::/**
+
+5
+*                 *  
+* *             * *  
+* * *         * * *  
+* * * *     * * * *  
+* * * * * * * * * *  
+* * * * * * * * * *  
+* * * *     * * * *  
+* * *         * * *  
+* *             * *  
+*                 *  
+
+
+Butterfly pattern
