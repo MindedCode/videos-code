066cd1594761051a708f798d2cdb38605458a8c9
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-13 15:11:43 +0530

    Update factorialReturn.java

--- Didi Shraddha/vid 7 - functions/with vid problems/factorialReturn.java
+++ Didi Shraddha/vid 7 - functions/with vid problems/factorialReturn.java
@@ -1,10 +1,17 @@
 // find the factorial of the number.
 
+// Make of func to Multiply 2 Nums & return the Product.
+
 import java.util.*;
 
 public class Main{
     public static void calculateFact(int n){
         
+        
+        if(n<0){
+            System.out.println("Invalid Numbers");
+            return;
+        }
         int factorical=1;
         for(int i=n; i>=1; i--){
             factorical = factorical*i;
@@ -20,6 +27,27 @@ public class Main{
         calculateFact(a);
     }
 }
+
+// import java.util.*;
+
+// public class Main{
+//     public static void calculateFact(int n){
+        
+//         int factorical=1;
+//         for(int i=n; i>=1; i--){
+//             factorical = factorical*i;
+//         }
+//         System.out.println(factorical);
+//         return;
+ 
+//     }
+//     public static void main (String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+        
+//         calculateFact(a);
+//     }
+// }
 
 **/O/p/**
 
