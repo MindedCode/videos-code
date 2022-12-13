b92917055fc08f3865ec887127327accb6f1322c
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-13 05:40:01 +0530

    Hallow butterfly pattern

---
+++ Didi Shraddha/vid6/hw2.java
@@ -0,0 +1,71 @@
+import java.util.*;
+ 
+public class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=i;j++){
+                if(i==j || j==1)
+                    System.out.print("* ");
+                else 
+                    System.out.print("  ");
+            }
+            // upper spaces
+            for(int j=2*(n-i); j>=1; j--  ){
+                System.out.print("  ");
+            }
+            // last upper inverted pyramid triangle
+            for(int j=1; j<=i; j++){
+                if(j==1 || j==i)
+                    System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println(" ");
+        }
+        // downpattern                                                           
+        for(int i=n; i>=1; i--  ){
+            for(int j=1;j<=i;j++){
+                if(i==j || j==1)
+                    System.out.print("* ");
+                else 
+                    System.out.print("  ");
+            }
+            // upper spaces
+            for(int j=2*(n-i); j>=1; j--  ){
+                System.out.print("  ");
+            }
+            // last upper inverted pyramid triangle
+            for(int j=1; j<=i; j++){
+                if(j==1 || j==i)
+                    System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println(" ");
+        }
+        
+        System.out.println("\n\nButterfly pattern");
+   }   
+}
+   
+
+
+
+// **/Output of the Program::/**
+
+// 6
+// *                     *  
+// * *                 * *  
+// *   *             *   *  
+// *     *         *     *  
+// *       *     *       *  
+// *         * *         *  
+// *         * *         *  
+// *       *     *       *  
+// *     *         *     *  
+// *   *             *   *  
+// * *                 * *  
+// *                     *  
+
+
+//Hollow Butterfly pattern
