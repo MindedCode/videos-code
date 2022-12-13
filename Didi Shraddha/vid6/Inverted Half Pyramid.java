fcc85fb7d6c7843fba69de05e90b53f6170477d8
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-13 06:44:44 +0530

    Inverted Half Pyramid number using for loop in java.

---
+++ Didi Shraddha/vid6/Inverted Half Pyramid.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+public class Main {
+  public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n=sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=i; j--)
+                    System.out.print(i + " ");
+
+            System.out.println();
+        }
+    }   
+}
+   
+
+
+
+// **/Output of the Program::/**
+
+// Print Inverted Half Pyramid.
+// 1 1 1 1 1
+//  2 2 2 2
+//   3 3 3
+//     4
+
+
+
+
+// Butterfly pattern
