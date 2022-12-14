2991fc50b6e468e76720bb69b36d210ff50c4ae5
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-14 06:57:33 +0530

    Create DoWhileInfiniteLoop.java

---
+++ Didi Shraddha/vid 8/DoWhileInfiniteLoop.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+Write an infinite loop using do while condition
+*******************************************************************************/
+
+import java.util.*;
+public class Main
+{
+    public static void DoWhileInfiniteLoop(){
+        int i = 0;
+       do{
+           i++;
+           System.out.print(i);
+       }while(i>0);
+       
+    }
+	public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        // int n = 0;
+        // int age = sc.nextInt();
+        
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        DoWhileInfiniteLoop();
+	}
+}
+
+"Output of the Program: "
+  
+1234567891011121314151617181920212223.......infinte loop.
