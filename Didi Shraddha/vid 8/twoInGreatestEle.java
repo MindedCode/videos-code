5c1d7b1a2dcce0215d74a893ce00d053a41b10a2
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-14 06:28:38 +0530

    Update twoInGreatestEle.java

--- Didi Shraddha/vid 8/twoInGreatestEle.java
+++ Didi Shraddha/vid 8/twoInGreatestEle.java
@@ -1,16 +1,15 @@
 /******************************************************************************
 Write a function which takes in 2 numbers and returns the greater of those two.
 *******************************************************************************/
+// 1st code
+
 import java.util.*;
 public class Main
 {
-    public static void twoInGreatestEle(int n1, int n2){
-        int result = 0;
-        if(n1==n2)
-        System.out.println("This is inputs equal");
-        else if(n1<n2)
-            System.out.println(n2 + " This number is greatest ");
-        else System.out.println( n1 +  " This number is greatest ");
+    public static int twoInGreatestEle(int n1, int n2){
+        if(n1<n2)
+            return n2;
+        else return n1;
     }
 	public static void main(String[] args) {
     
@@ -19,12 +18,45 @@ public class Main
         int b = sc.nextInt();
         // int c = sc.nextInt();
         
-        twoInGreatestEle(a,b);
+        System.out.println("Element is Greatest " + twoInGreatestEle(a,b));
 	}
 }
 
-"Output of the program"
+"Output :"
+456
+567
+Element is Greatest 567
+
+
+
+
+// 2nd code no return answer--------------------------------------------
+
+
+// import java.util.*;
+// public class Main
+// {
+//     public static void twoInGreatestEle(int n1, int n2){
+//         int result = 0;
+//         if(n1==n2)
+//         System.out.println("This is inputs equal");
+//         else if(n1<n2)
+//             System.out.println(n2 + " This number is greatest ");
+//         else System.out.println( n1 +  " This number is greatest ");
+//     }
+// 	public static void main(String[] args) {
+    
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         // int c = sc.nextInt();
+        
+//         twoInGreatestEle(a,b);
+// 	}
+// }
+
+// "Output of the program"
   
- 45
- 67
- 67 This number is Greatest
+//  45
+//  67
+//  67 This number is Greatest
