871a88ede7972b975b7ecf5a7812472a56411997
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-14 06:34:55 +0530

    Create findCircumfOfCircle.java

---
+++ Didi Shraddha/vid 8/findCircumfOfCircle.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+Write a function that takes in the radius as input and returns the circumference of a circle.
+C=2πr
+*******************************************************************************/
+
+import java.util.*;
+public class Main
+{
+    public static int findCircumfOfCircle(int n1){
+        
+       finally float pi = 3.14f;
+       int c = 2*pi*n1;
+       return c;
+       
+    }
+	public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        finally float pi = 3.14f;
+        
+        
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        
+        System.out.println("Element is Greatest " + findCircumfOfCircle(a,b));
+	}
+}
