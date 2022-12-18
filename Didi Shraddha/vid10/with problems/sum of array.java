7dbc63c3023c658b89bc80db19db27d032d03287
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-18 08:37:01 +0530

    Create sum of array.java

---
+++ Didi Shraddha/vid10/with problems/sum of array.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int marks[] = {1,2,3,4,5};
+	    int result  = 0;
+	    int key = 5;
+	    for(int i=0; i<5; i++){
+	         if(key == marks[i])
+	             System.out.println(i);
+	    }
+	}
+}
+
+
+"Output of the array"
+  
+  4
