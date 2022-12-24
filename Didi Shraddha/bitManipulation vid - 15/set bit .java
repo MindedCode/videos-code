bb9ab45b47d68dafa9d19f30682d45c490012a9f
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-25 00:57:18 +0530

    Create set bit .java

---
+++ Didi Shraddha/bitManipulation vid - 15/set bit .java
@@ -0,0 +1,25 @@
+//Set 2nd bit (position = 1) of a  number n. n = 0101  numbering 3 2 1 0 
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // int t = 1;
+        // while(t<){
+        int n = sc.nextInt();
+        int pos = sc.nextInt();
+        
+        int bitMask = 1<<pos;
+         int ans = bitMask|n;
+         System.out.println(ans);
+     
+    }
+}
+
+Output----------------
+
+5
+1
+7
