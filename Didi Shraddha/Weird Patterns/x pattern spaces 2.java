dc8802f25dd70f383ac771418f1350638f667e75
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-01-02 06:46:55 +0530

    Create x pattern spaces 2.java

---
+++ Didi Shraddha/Weird Patterns/x pattern spaces 2.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class Main{
+    
+    
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            
+            // space
+            for(int j=2; j<=2*i; j++){
+                    System.out.print(" ");
+            }
+                
+            for(int j=1;j<=n;j++){
+                if(j<n-i+1)
+                    System.out.print("*");
+            } 
+             // upper space among
+        // for(int i=1;i<=n;i++){
+            
+            // space
+            // for(int j=2*n; j>=1; j--){
+            //     if(j<2*i-1)
+            //         System.out.print(" ");
+            // }
+            for(int j=2*(n-i); j>=1; j--){
+                System.out.print(" ");
+            }
+                
+            for(int j=1;j<=n;j++){
+                if(j<n-i+1)
+                    System.out.print("*");
+            }System.out.println();
+        
+        }
+        
+       
+        
+        // 2st part ka second part 
+        
+        for(int i=n;i>=1;i--){
+            
+            // space
+            for(int j=2; j<=2*i; j++){
+                    System.out.print(" ");
+            }
+                
+            for(int j=1;j<=n;j++){
+                if(j<n-i+1)
+                    System.out.print("*");
+            } 
+             // upper space among
+        // for(int i=1;i<=n;i++){
+            
+            // space
+            // for(int j=2*n; j>=1; j--){
+            //     if(j<2*i-1)
+            //         System.out.print(" ");
+            // }
+            for(int j=2*(n-i); j>=1; j--){
+                System.out.print(" ");
+            }
+                
+            for(int j=1;j<=n;j++){
+                if(j<n-i+1)
+                    System.out.print("*");
+            }System.out.println();
+        
+        }
+        
+    }
+}
+
+/*Output of the Program */
+6
+ *****          *****
+   ****        ****
+     ***      ***
+       **    **
+         *  *
+           
+           
+         *  *
+       **    **
+     ***      ***
+   ****        ****
+ *****          *****
