e6a433d571d7d6d9ced580020255ee263037c535
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-01-25 20:04:20 +0530

    Create Collection Stack.java

---
+++ Didi Shraddha/Java Part 2/Stack/Collection Stack.java
@@ -0,0 +1,24 @@
+// only using main function  Stack Collection Frame work
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
+
+
+Output is :
+4
+  3
+  2
+  1
