656705d6e64ba75926a0d69f87f2142c7fc378e0
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-02-02 05:18:46 +0530

    Create basic  class.java

---
+++ Full Stack Deve/opps in java/basic  class.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void drinking(){
+        System.out.println("drinking Syrp");
+    }
+
+    public static void main (String[] args) {
+        Main p1 = new Main();
+        
+        String name = "Minded Code";
+        System.out.println(name);
+        p1.drinking();
+    }
+}
+
+// Output is:
+Minded Code
+drinking Syrp
