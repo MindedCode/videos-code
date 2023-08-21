17904e42f68505e99c807c27d86b1703f8584870
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-08-22 00:23:24 +0530

    Create Hello.cs
    Using Console.WriteLine() function

---
+++ DotNET/Hello.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Minded
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(3 + 4);
+            Console.Write(6 + 4);
+            Console.WriteLine(3 + 4);
+            Console.WriteLine(3 - 4);
+            Console.WriteLine("Hii Genesh");
+        }
+    }
+}
